4b4a953514954d7ca23ee928ccce82c4048871ec
Colin Millar <colinpmillar@gmail.com>
2025-07-16 14:42:40 +0200

    fetch data

--- boot/assessment_data.R
+++ boot/assessment_data.R
@@ -1,2 +1,11 @@
 # fetch data from online storage
 
+library(TAF)
+
+tag <- "2025v1"
+
+url <- paste0("https://github.com/ices-taf/OHAT_slim/releases/download/", tag, "/")
+
+download(paste0(url, "biota.rds"))
+download(paste0(url, "sediment.rds"))
+download(paste0(url, "water.rds"))
